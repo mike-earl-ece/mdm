7abae45b81dabba49b44b0cf0dc8e5380d55024f
dfroslie-ldi <dfroslie-ldi>
2024-09-11 17:00:04 +0000

    Add meter metrics scripts

---
+++ transformMDM_DailyHourlyAggregation.py
@@ -0,0 +1,63 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Transform MDM Daily Hourly Aggregation
+# MAGIC Aggregate data to the day and hour ending across all data points for both AMI and VEE values.  This will enable heat maps and other Power BI reporting.
+
+# COMMAND ----------
+
+from delta.tables import *
+from pyspark.sql.functions import min, max, sum, year, month, hour, day
+
+debug=1
+
+# COMMAND ----------
+
+# Set up storage account connection and initialize variables.
+storage_end_point = "ecemdmstore.dfs.core.windows.net" 
+my_scope = "MeterDataStorageVault"
+my_key = "MeterStorageAccessKey"
+
+spark.conf.set(
+    "fs.azure.account.key." + storage_end_point,
+    dbutils.secrets.get(scope=my_scope, key=my_key))
+
+uri = "abfss://meter-data@ecemdmstore.dfs.core.windows.net/"
+
+output_path = uri + "Gold/MeterMetrics/DailyHourlyAggregation"
+
+source_table_name = "MeterData3"
+
+# COMMAND ----------
+
+#Read the data from the source table
+meter_df = spark.read.table(source_table_name)
+
+# Ignore invalid rows
+meter_df_cleaned = meter_df.filter(meter_df.MeterNumber.isNotNull())
+
+if debug:
+    display(meter_df_cleaned)
+
+
+# COMMAND ----------
+
+# Aggregate to hourly for each day.
+meter_df_cleaned = meter_df_cleaned.withColumn("Year", year(meter_df_cleaned.EndDateTime))\
+                            .withColumn("Month", month(meter_df_cleaned.EndDateTime))\
+                            .withColumn("Day", day(meter_df_cleaned.EndDateTime))\
+                            .withColumn("HourEnding", hour(meter_df_cleaned.EndDateTime))
+
+daily_hourly_df = meter_df_cleaned.groupBy("Year", "Month", "Day", "HourEnding").agg(sum("AMIValue").alias("TotalAMIValue"), 
+                                                                                     sum("VEEValue").alias("TotalVEEValue"))
+
+if debug:
+    display(daily_hourly_df)
+
+# COMMAND ----------
+
+# Write out to delta format.
+daily_hourly_df.write.mode('overwrite').format("delta").save(output_path)
+
+# Clean up old files
+delta_table = DeltaTable.forPath(spark, output_path)
+delta_table.vacuum()  
---
+++ transformMDM_HourlyAggregation.py
@@ -0,0 +1,64 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Transform MDM Hourly Aggregation
+# MAGIC Aggregate data to the hour ending across all data points for both AMI and VEE values.  Calculate AMI vs. VEE differences for each hour.
+
+# COMMAND ----------
+
+from delta.tables import *
+from pyspark.sql.functions import min, max, sum, hour
+
+debug=1
+
+# COMMAND ----------
+
+# Set up storage account connection and initialize variables.
+storage_end_point = "ecemdmstore.dfs.core.windows.net" 
+my_scope = "MeterDataStorageVault"
+my_key = "MeterStorageAccessKey"
+
+spark.conf.set(
+    "fs.azure.account.key." + storage_end_point,
+    dbutils.secrets.get(scope=my_scope, key=my_key))
+
+uri = "abfss://meter-data@ecemdmstore.dfs.core.windows.net/"
+
+output_path = uri + "Gold/MeterMetrics/HourlyAggregation"
+
+source_table_name = "MeterData3"
+
+# COMMAND ----------
+
+#Read the data from the source table
+meter_df = spark.read.table(source_table_name)
+
+# Ignore invalid rows
+meter_df_cleaned = meter_df.filter(meter_df.MeterNumber.isNotNull())
+
+if debug:
+    display(meter_df_cleaned)
+
+
+# COMMAND ----------
+
+# Aggregate to hourly
+meter_df_cleaned = meter_df_cleaned.withColumn("HourEnding", hour(meter_df_cleaned.EndDateTime))
+
+hourly_df = meter_df_cleaned.groupBy("HourEnding").agg(sum("AMIValue").alias("TotalAMIValue"), sum("VEEValue").alias("TotalVEEValue"))
+
+hourly_df = hourly_df.withColumn("AMI_VEE_kWhDifference", (hourly_df.TotalVEEValue - hourly_df.TotalAMIValue))
+hourly_df = hourly_df.withColumn("AMI_VEE_PercentDifference", (hourly_df.TotalVEEValue - hourly_df.TotalAMIValue) / hourly_df.TotalAMIValue)
+
+if debug:
+    display(hourly_df)
+
+
+
+# COMMAND ----------
+
+# Write out to delta format.
+hourly_df.write.mode('overwrite').format("delta").save(output_path)
+
+# Clean up old files
+delta_table = DeltaTable.forPath(spark, output_path)
+delta_table.vacuum()  
