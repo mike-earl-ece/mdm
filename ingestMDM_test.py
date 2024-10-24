7178587ecde8eb69ab97191a6f0d482112610466
dfroslie-ldi <dfroslie-ldi>
2024-10-24 14:47:54 +0000

    Update ingestMDM to include a post ingest cleaning step.  Add ingestMDM_test for exploration.

--- ingestMDM_test.py
+++ ingestMDM_test.py
@@ -1,8 +1,4 @@
 # Databricks notebook source
-debug = 1
-
-# COMMAND ----------
-
 storage_end_point = "ecemdmstore.dfs.core.windows.net" 
 my_scope = "MeterDataStorageVault"
 my_key = "MeterStorageAccessKey"
@@ -11,20 +7,18 @@ spark.conf.set(
     "fs.azure.account.key." + storage_end_point,
     dbutils.secrets.get(scope=my_scope, key=my_key))
 
-uri = "abfss://meter-data@ecemdmstore.dfs.core.windows.net/"
-checkpoint_path = uri + "Bronze/Checkpoint"
+uri = "abfss://meter-data-test@ecemdmstore.dfs.core.windows.net/"
+checkpoint_path = uri + "Output/_checkpoint"
 
-input_path = uri + "Bronze/"
-output_path = uri + "Bronze6/Output"
+input_path = uri + "Input/"
+output_path = uri + "Output"
 
-table_name = "MeterData3"
+table_name = "MeterData999"
 
 
 # COMMAND ----------
 
-
-if debug:
-    dbutils.fs.ls(input_path)
+dbutils.fs.ls(input_path)
 
 # COMMAND ----------
 
@@ -54,6 +48,57 @@ schema = StructType([
 
 # COMMAND ----------
 
+
+
+# COMMAND ----------
+
+#spark.sql(f"CREATE TABLE IF NOT EXISTS default.{table_name} USING DELTA")
+spark.sql(f"CREATE TABLE IF NOT EXISTS default.meterdata999 USING DELTA")
+
+
+# COMMAND ----------
+
+from delta.tables import *
+
+deltaTable = DeltaTable.forName(spark, table_name)
+
+#deltaTable = DeltaTable.forName(spark, "default.meterdata3")
+
+
+# COMMAND ----------
+
+
+# Function to upsert microBatchOutputDF into Delta table using merge
+def upsertToDelta(microBatchOutputDF, batchId):
+  (deltaTable.alias("t").merge(
+      microBatchOutputDF.alias("s"),
+      "s.key = t.key")
+    .whenMatchedUpdateAll()
+    .whenNotMatchedInsertAll()
+    .execute()
+  )
+
+  df1 = spark.readStream.format("cloudFiles") \
+    .schema(schema) \
+    .option("cloudFiles.format", "csv") \
+    .option("rescuedDataColumn", "_rescued_data") \
+    .option("cloudFiles.schemaLocation", checkpoint_path ) \
+    .load(input_path) 
+
+query = df1.writeStream \
+    .format("delta") \
+    .option("checkpointLocation", checkpoint_path) \
+    .option("path", output_path) \
+    .foreachBatch(upsertToDelta) \
+    .outputMode("update") \
+    .trigger(availableNow=True) \
+    .table(table_name)    
+
+query.awaitTermination()
+
+
+# COMMAND ----------
+
 df1 = spark.readStream.format("cloudFiles") \
     .schema(schema) \
     .option("cloudFiles.format", "csv") \
@@ -75,34 +120,26 @@ query.awaitTermination()
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ## Data cleaning
-# MAGIC The following section takes the following actions to prep the data for further processing:
-# MAGIC - Remove the header row that got ingested from each file.
-# MAGIC - Removes the _rescued_data column.  This column has information on the ingestion source for each data point.
-# MAGIC - Drops duplicate rows to handle the situation when the same data points are ingested multiple times.
-# MAGIC
-# MAGIC The resulting data set is saved to the Silver folder in delta format.
-
-# COMMAND ----------
-
 from pyspark.sql.functions import min, max
 
 df2 = spark.read.table(table_name)
 
-if debug:
-    print(df2.count())
-
 df2_cleaned = df2.filter(df2.MeterNumber.isNotNull())
-df2_cleaned = df2.drop('_rescued_data')
-df2_cleaned = df2_cleaned.dropDuplicates()
 
-if debug:
-    print(df2_cleaned.count())
-    display(df2_cleaned.agg(min('StartDateTime').alias('FirstSampleTime'), max('EndDateTime').alias('LastSampleTime')))
+print(df2_cleaned.count())
+
+display(df2_cleaned.agg(min('StartDateTime').alias('FirstSampleTime'), max('EndDateTime').alias('LastSampleTime')))
+
+display(df2_cleaned)
 
 # COMMAND ----------
 
-# Save the data to the silver tier.  
-# ToDo:  investigate update patterns as an alternative to overwrite.
-df2_cleaned.write.format("delta").mode("overwrite").save(uri + "Silver/Conformed")
+df2_cleaned_nometadata = df2_cleaned.drop("_rescued_data")
+df2_duplicates = df2_cleaned_nometadata.groupBy(df2_cleaned_nometadata.columns).count().filter("count > 1").drop("count")
+
+print(df2_duplicates.count())
+display(df2_duplicates)
+
+# COMMAND ----------
+
+df2_cleaned = df2_cleaned.dro
