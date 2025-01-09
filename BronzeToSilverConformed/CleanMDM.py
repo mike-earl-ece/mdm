eae3ec4756eea9ff3a93242005a7ec5f75fbc6cb
dfroslie-ldi <dfroslie-ldi>
2025-01-09 14:36:42 +0000

    Added duplicate data checks.

--- BronzeToSilverConformed/CleanMDM.py
+++ BronzeToSilverConformed/CleanMDM.py
@@ -6,7 +6,7 @@
 # COMMAND ----------
 
 # Imports and debug
-from pyspark.sql.functions import lit, to_timestamp, max
+from pyspark.sql.functions import lit, to_timestamp, max, col, when
 from pyspark.sql.utils import AnalysisException
 from delta.tables import DeltaTable
 
@@ -36,7 +36,7 @@ if debug:
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Data cleaning
+# MAGIC ## Incremental data update
 # MAGIC The following section updates the MDM data in the SilverConformed tier based new data that has come into the Bronze ingestion table since the last update to SilverConformed.  Specifically:
 # MAGIC - Use the change history of the SilverConformed table to get the last change date.
 # MAGIC - Use the change history of the Bronze table to get all changes since the last SilverConformed change date.
@@ -82,13 +82,18 @@ if debug and clean_has_data:
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ### Get the new data from the bronze ingest table and do some cleaning.
+
+# COMMAND ----------
+
 
 # Find the changes in the Bronze ingest table.  
 found_ingest_changes = True # Will be set to False if no changes.
 
 # If the clean table has no data, get all the changes.
 if clean_has_data == False:
-        ingest_changes_df = spark.read \
+        ingest_changes_all_df = spark.read \
                 .option("readChangeFeed", "true") \
                 .option("startingVersion", 0) \
                 .table(input_table_name)
@@ -96,7 +101,7 @@ if clean_has_data == False:
 # thrown, caught, and found_changes will be set to False.
 else:
         try:
-                ingest_changes_df = spark.read \
+                ingest_changes_all_df = spark.read \
                         .option("readChangeFeed", "true") \
                         .option("startingTimestamp", last_change) \
                         .table(input_table_name)
@@ -107,18 +112,81 @@ else:
 
 if debug:
         if found_ingest_changes:
-                display(ingest_changes_df)
+                display(ingest_changes_all_df)
         else:
                 print("No ingest changes found.")
 
+# No need to continue if there are no ingest changes found.
+if found_ingest_changes == False:
+        dbutils.notebook.exit("No ingest changes found.") 
 
 # COMMAND ----------
 
-# Drop the change tracking information from the ingest changes.
-if found_ingest_changes:
-    ingest_changes_df = ingest_changes_df.drop("_rescued_data", "_change_type", "_commit_version", "_commit_timestamp") 
-    if debug:
-        display(ingest_changes_df)
+# When an update happens on the input table, there are two rows added to the change list - one representing the new row and one representing the old row.  
+# We need to remove the old row from the change set by filtering out _change_types with the values update_preimage.
+ingest_changes_all_df = ingest_changes_all_df.filter(col('_change_type') != "update_preimage")
+
+if debug:
+  print(ingest_changes_all_df.count())
+
+# COMMAND ----------
+
+# MDM extract appears to have some duplication issues. Deal with that here.
+
+# First, there are some duplicates in the data, so drop these after removing the metadata columns.
+ingest_changes_df = ingest_changes_all_df.drop("_rescued_data", "_change_type", "_commit_version", "_commit_timestamp") 
+ingest_changes_df = ingest_changes_df.dropDuplicates()
+
+if debug:
+  print(ingest_changes_df.count())
+
+# Second, there are some rows that have duplicate metadata columns (see below), but with an AMIValue equal to 0 and a null VEEValue.  These rows need to be removed.
+# Find duplicates.
+subset_df = ingest_changes_df.select("MeterNumber", "UnitOfMeasure", "FlowDirection", "Channel", "StartDateTime", "EndDateTime")
+duplicates_df = subset_df.groupBy("MeterNumber", "UnitOfMeasure", "FlowDirection", "Channel", "StartDateTime", "EndDateTime").count().filter("count > 1")
+if debug:
+  print(duplicates_df.count())
+
+if duplicates_df.count() > 0:
+  # Join back to main dataset; the count column will track duplicates.
+  ingest_changes_dup_df = ingest_changes_df.join(duplicates_df, ["MeterNumber", "UnitOfMeasure", "FlowDirection", "Channel", "StartDateTime", "EndDateTime"], "left")
+
+  # Deal with null count values as they seem to have strange behavior in logic statements.
+  ingest_changes_dup_df = ingest_changes_dup_df.withColumn("DupCount", when(col('count').isNull(), 1).otherwise(col('count')))
+
+  if debug:
+    print(ingest_changes_dup_df.count())
+
+  # Handle the case where the VEEValue is null in the duplicate set.  
+  ingest_valid_df = ingest_changes_dup_df.filter(~( (col('VEEValue').isNull()) & (col('DupCount') == 2) ) )
+
+  # Now handle the case where the AMIValue is 0 and the VEEValue is 0 in the duplicate set.
+  ingest_valid_df = ingest_valid_df.filter(~( (col('VEEValue') == 0) & (col('AMIValue') == 0) & (col('DupCount') == 2) ) )
+
+  ingest_valid_df = ingest_valid_df.drop('count', 'DupCount')
+
+  if debug:
+    print(ingest_valid_df.count())
+    display(ingest_valid_df)  
+else:
+  ingest_valid_df = ingest_changes_df
+
+# COMMAND ----------
+
+ # Check for duplicates at this point,  If any exist, display some debug information and abort.
+duplicates_df = ingest_valid_df.groupBy("MeterNumber", "UnitOfMeasure", "FlowDirection", "Channel", "StartDateTime", "EndDateTime")\
+                  .count().filter("count > 1")
+  
+if duplicates_df.count() > 0:
+    print("Duplicates still exist: " + str(duplicates_df.count()))
+    # Join with the original dataset and display. 
+    ingest_changes_dup2_df = duplicates_df.join(ingest_changes_all_df, ["MeterNumber", "UnitOfMeasure", "FlowDirection", "Channel", "StartDateTime", "EndDateTime"], "left")
+    print(ingest_changes_dup2_df.count())
+    display(ingest_changes_dup2_df)
+
+    raise Exception("CleanMDM: Duplicates exist in data after cleaning. Aborting script. Please review the data and correct the issue.")
+else:
+  print("No duplicates found.")
 
 # COMMAND ----------
 
@@ -150,13 +218,28 @@ if found_ingest_changes:
 
 # COMMAND ----------
 
-if debug:
-    clean_out_df = spark.read.table(output_table_name)
-    print(clean_out_df.count())
-    display(clean_out_df)
+clean_out_df = spark.read.table(output_table_name)
+print(clean_out_df.count())
+display(clean_out_df)
 
 # COMMAND ----------
 
-# Look for duplicate rows.  This likely indicates a problem with the upsert.
-if debug:
-    display(clean_out_df.groupBy(clean_out_df.columns).count().filter("count > 1").orderBy("count", ascending=False))
+ # Check for duplicates at this point,  This likely indicates a problem with the upsert.  Abort if there is an issue.
+duplicates_out_df = clean_out_df.groupBy("MeterNumber", "UnitOfMeasure", "FlowDirection", "Channel", "StartDateTime", "EndDateTime")\
+                  .count().filter("count > 1")
+
+dup_count = duplicates_out_df.count()
+
+if dup_count > 0: 
+  print(dup_count)
+  display(duplicates_out_df)
+
+  raise Exception("Duplicates found in the clean table.  Please investigate.")
+
+else:
+  print("No duplicates found after the upsert.")
+
+# COMMAND ----------
+
+# Clean up history
+clean_table.vacuum()
